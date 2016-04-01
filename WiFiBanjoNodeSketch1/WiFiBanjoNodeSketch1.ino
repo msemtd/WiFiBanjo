f6a127aa50b4aa73097b8dbcc1fa07733a8c2752
Michael Erskine <michael.erskine@ketech.com>
2016-04-01 13:55:14 +0100

    Signed-off-by: Michael Erskine <michael.erskine@ketech.com>

--- WiFiBanjoNodeSketch1/WiFiBanjoNodeSketch1.ino
+++ WiFiBanjoNodeSketch1/WiFiBanjoNodeSketch1.ino
@@ -1,17 +1,24 @@
 #include <ESP8266WiFi.h>
 #include <DNSServer.h>
+
 #include <ESP8266WebServer.h>
 
 const byte DNS_PORT = 53;
 IPAddress apIP(192, 168, 1, 1);
 DNSServer dnsServer;
 ESP8266WebServer webServer(80);
+const char *myHostname = "banjo";
 
 String responseHTML = ""
   "<!DOCTYPE html><html><head><title>Toby's Banjo - Captive Portal</title></head><body>"
   "<h1>Hello There!</h1><p>You are connected to Toby's Banjo WiFi Access Point.</p></body></html>";
 
 void setup() {
+  delay(1000);
+  Serial.begin(9600);
+  Serial.println();
+  Serial.print("Configuring access point...");
+  
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
   WiFi.softAP("Toby's Banjo");
